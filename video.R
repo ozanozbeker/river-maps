ecf59ba3d47296518bdeba21223b7b8c473662b3
Ozan Ozbeker <ozanozbeker99@outlook.com>
2024-07-26 07:39:52 -0400

    updates

--- video.R
+++ video.R
@@ -1,31 +1,32 @@
 # 1 | Libraries ----
-install.packages(
-  "remotes"
-)
+# remotes::install_version(
+#   "rayshader",
+#   version = "0.35.7",
+#   repos = "http://cran.us.r-project.org"
+# )
+#
+# remotes::install_version(
+#   "rayimage",
+#   version = "0.9.1",
+#   repos = "http://cran.us.r-project.org"
+# )
+#
+# remotes::install_version(
+#   "rayvertex",
+#   version = "0.7.9",
+#   repos = "http://cran.us.r-project.org"
+# )
+#
+# remotes::install_version(
+#   "rayrender",
+#   version = "0.29.6",
+#   repos = "http://cran.us.r-project.org"
+# )
 
-remotes::install_version(
-  "rayshader",
-  version = "0.35.7",
-  repos = "http://cran.us.r-project.org"
-)
-
-remotes::install_version(
-  "rayimage",
-  version = "0.9.1",
-  repos = "http://cran.us.r-project.org"
-)
-
-remotes::install_version(
-  "rayvertex",
-  version = "0.7.9",
-  repos = "http://cran.us.r-project.org"
-)
-
-remotes::install_version(
-  "rayrender",
-  version = "0.29.6",
-  repos = "http://cran.us.r-project.org"
-)
+devtools::install_github("tylermorganwall/rayshader")
+devtools::install_github("tylermorganwall/rayimage")
+devtools::install_github("tylermorganwall/rayvertex")
+devtools::install_github("tylermorganwall/rayrender")
 
 library(tidyverse)
 library(fs)
@@ -37,29 +38,40 @@ library(rayshader)
 
 state = "West Virginia"
 
-# 2 | State Borders ----
+# 2 | Download Files
+url_rivers = "https://data.hydrosheds.org/file/HydroRIVERS/HydroRIVERS_v10_na_shp.zip"
+file_rivers = str_c("data/", path_file(url_rivers))
+
+url_hdri = "https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/4k/photo_studio_loft_hall_4k.hdr"
+file_hdri = str_c("data/", path_file(url_hdri))
+
+if (FALSE) {
+  download.file(
+    url = url_rivers,
+    destfile = file_rivers,
+    mode = "wb"
+  )
+
+  unzip(file_rivers, exdir = "data")
+
+  dir_info("data") |>
+    filter(path |> str_ends(".zip")) |>
+    pull("path") |>
+    file_delete()
+
+  download.file(
+    url = url_hdri,
+    destfile = file_hdri,
+    mode = "wb"
+  )
+}
+
+# 3 | State Borders ----
 state_sf = gadm(country = "USA", level = 1, path = "data") |>
   st_as_sf() |>
   filter(NAME_1 == state)
 
 plot(st_geometry(state_sf))
-
-# 3 | Download Rivers ----
-url_rivers = "https://data.hydrosheds.org/file/HydroRIVERS/HydroRIVERS_v10_na_shp.zip"
-file_rivers = str_c("data/", path_file(url_rivers))
-
-download.file(
-  url = url_rivers,
-  destfile = file_rivers,
-  mode = "wb"
-)
-
-unzip(file_rivers, exdir = "data")
-
-dir_info("data") |>
-  filter(path |> str_ends(".zip")) |>
-  pull("path") |>
-  file_delete()
 
 # 4 | Load Rivers ----
 filename = dir_info("data", recurse = 1) |>
@@ -94,19 +106,21 @@ state_river_width = state_river |>
     width = case_when( # in pixels
       # ORD_FLOW == 1 ~ 20,
       # ORD_FLOW == 2 ~ 18,
-      ORD_FLOW == 3 ~ 12,
-      ORD_FLOW == 4 ~ 10,
-      ORD_FLOW == 5 ~  8,
-      ORD_FLOW == 6 ~  6,
-      ORD_FLOW == 7 ~  4,
+      ORD_FLOW == 3 ~ 16,
+      ORD_FLOW == 4 ~ 14,
+      ORD_FLOW == 5 ~ 12,
+      ORD_FLOW == 6 ~ 10,
+      ORD_FLOW == 7 ~  8,
       .default = 0)) |>
   st_as_sf() |>
   st_transform(crs = crs_na)
 
+rm(state_river)
+
 # 6 | DEM ----
 dem = get_elev_raster(
   locations = state_sf,
-  z = 9,
+  z = 10,
   clip = "locations"
 )
 
@@ -119,7 +133,7 @@ dem_matrix = raster_to_matrix(dem_state)
 # 7 | Render Scene ----
 dem_matrix |>
   height_shade(
-    texture = colorRampPalette(c("#003f5c","#00202e"))(128)) |>
+    texture = colorRampPalette(c("#003f5C","#00121A"))(128)) |>
   add_overlay(
     generate_line_overlay(
       geometry = state_river_width,
@@ -131,39 +145,31 @@ dem_matrix |>
     alphalayer = 1) |>
   plot_3d(
     dem_matrix,
-    zscale = 20,
+    zscale = 7,
     solid = FALSE,
     shadow = TRUE,
     shadow_darkness = 1,
     background = "white",
-    windowsize = c(200, 200, 2160, 2160),
-    zoom = 0.7,
+    windowsize = c(200, 200, 800, 800),
+    zoom = 0.55,
     phi = 85,
-    theta = 0
-  )
+    theta = 0)
+
+rgl::snapshot3d(paste0("west-virginia-2d-rivers_v2.png"))
 
 # 8 | Render Object ----
-file_name = "west-virginia-3d-elevation-rivers.png"
-
-url_hdri = "https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/4k/photo_studio_loft_hall_4k.hdr"
-file_hdri = str_c("data/", path_file(url_hdri))
-
-download.file(
-  url = url_hdri,
-  destfile = file_hdri,
-  mode = "wb"
-)
+file_name = str_c(state, " 3D Elevation Map with Rivers.png")
 
 render_highquality(
   filename = file_name,
-  samples = 256,
+  # samples = 256,
+  # sample_method = "sobol",
   preview = TRUE,
   light = FALSE,
   environment_light = file_hdri,
+  intensity_env = 1.5,
   interactive = FALSE,
   progress = TRUE,
-  width = 2160,
-  height = 2160,
-  parallel = TRUE,
-  lightdirection = 225
+  width = 3000,
+  height = 2400
 )
